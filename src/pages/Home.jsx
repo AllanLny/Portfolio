e4d873b21d0b4b967d2e141599fb8b8c26c52891
AllanLny <allan1411lannoy@gmail.com>
2025-08-21 23:53:34 +0200

    fix(front): fix linter issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import Projects from '../components/Projects';
 import Recommendations from '../components/Recommendations';
 import './Home.scss';
